a43bd2250e736a23cb03af8ccf1d4b80a63c7dbc
emericgautier <emericgtr@gmail.com>
2022-07-22 14:18:47 +0200

    createElement

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert("Hello!");
+const bubble = document.createElement("span");
+bubble.classList.add("bubble");
+document.body.appendChild(bubble);
+console.log(bubble);
+
+// console.log(Math.random() * 200 + 100);
+const size = Math.random() * 200 + 100 + "px";
+// console.log(size);
+
+bubble.style.height = size;
+bubble.style.width = size;
